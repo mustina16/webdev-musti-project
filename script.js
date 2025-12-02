f66dd089468f3180be587f7566ff15bbb7b0e22e
mustina saputri <mustina291298@gmail.com>
2025-12-02 09:10:52 +0700

    update kecil di Index.html untuk testing

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector("h1");
+    const nav = document.querySelector("nav");
+    const mainContent = document.querySelector("main");
+    const aside = document.querySelector("aside");
+
+    title.textContent = "Belajar Javascript Dasar - Interaksi Pertama! ☠ "
+    title.addEventListener("click", () => {
+        nav.classList.toggle("show-nav"); 
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if(event.key === "h") {
+            document.body.classList.toggle("dark");
+        }
+    });
+
+    let count = 0;
+    mainContent.addEventListener("click", () => {
+        count++;
+        aside.textContent = `kamu klik konten ini sebanyak ${count} kali`;
+    })
+});
